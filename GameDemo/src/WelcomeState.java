669210e708a1bd5b116ef1e483af4c54327346c5
Maayan Cohen <maayanch98@gmail.com>
2023-01-20 14:37:10 +0200

    design home page

--- GameDemo/src/WelcomeState.java
+++ GameDemo/src/WelcomeState.java
@@ -1,6 +1,12 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class WelcomeState extends GameState {
 
@@ -26,10 +32,30 @@ public class WelcomeState extends GameState {
 	public void render(GameFrameBuffer aGameFrameBuffer) {
 		Graphics g = aGameFrameBuffer.graphics();
 		
-		String text = "PRESS ANY KEY TO PLAY";
-		int textWidth = g.getFontMetrics().stringWidth(text);
+		
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File("homePage.png"));
+			g.drawImage(image, 0, 0, aGameFrameBuffer.getWidth(), aGameFrameBuffer.getHeight(), null);
+		} catch (IOException e) {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, aGameFrameBuffer.getWidth(), aGameFrameBuffer.getHeight());
+		}
+		
+		g.setColor(Color.YELLOW);
+		g.fillRect(80, 275, 80, 40);
+		g.fillRect(280, 275, 80, 40);
+		g.fillRect(480, 275, 80, 40);
+		String chooseLevel = "please choose your level";
+		int textWidth = g.getFontMetrics().stringWidth(chooseLevel);
 		g.setColor(Color.white);
-		g.drawString(text, (aGameFrameBuffer.getWidth()-textWidth)/2, aGameFrameBuffer.getHeight()/2);
+		g.drawString(chooseLevel, (aGameFrameBuffer.getWidth()/2-textWidth/2), aGameFrameBuffer.getHeight()/2);
+		g.setColor(Color.BLACK);
+		g.drawString("easy", 105,300 );
+		g.drawString("medium", 300,300 );
+		g.drawString("Hard", 505,300 );
 
+
+		
 	}
 }
